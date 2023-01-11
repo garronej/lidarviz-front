b3434ea25d8470456a84bb3115ccf68152eb9afa
Maxime Allain <maxime.allain.8@gmail.com>
2023-01-11 19:44:59 +0100

    feat: draw polygon when city is selected

--- src/hooks/useMap.tsx
+++ src/hooks/useMap.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
-import { Map, View } from "ol";
+import { Feature, Map, View } from "ol";
 import { fromLonLat, Projection } from "ol/proj";
 import { getIgnTileLayer } from "../map/ignTileLayer";
 import { zoomController, positionCurseurController } from "../map/controllers";
 import { Polygon } from "ol/geom";
 import { Coordinate } from "ol/coordinate";
+import { Vector as VectorLayer } from "ol/layer";
+import VectorSource from "ol/source/Vector";
 
 const useMap = (target: string, center: [number, number], zoom: number) => {
   const [view, setView] = useState<View | undefined>(undefined);
+  const [map, setMap] = useState<Map | undefined>(undefined);
 
   const ignTileLayer = getIgnTileLayer();
 
@@ -23,6 +26,7 @@ const useMap = (target: string, center: [number, number], zoom: number) => {
       view: initialView,
       controls: [zoomController, positionCurseurController],
     });
+    setMap(map);
     setView(initialView);
 
     return () => map.setTarget(undefined);
@@ -39,12 +43,15 @@ const useMap = (target: string, center: [number, number], zoom: number) => {
   const fitViewToPolygon = (coordinates: Coordinate[][]) => {
     const epsg4326 = new Projection({ code: "EPSG:4326" });
     const epsg3857 = new Projection({ code: "EPSG:3857" });
-    const polygon = new Polygon(coordinates).transform(
-      epsg4326,
-      epsg3857
-    ) as Polygon;
+    // TODO handle multi-polygon like Marseille
+    const polygon = new Polygon(coordinates).transform(epsg4326, epsg3857);
 
-    view?.fit(polygon);
+    view?.fit(polygon as Polygon, { padding: [150, 150, 150, 150] });
+
+    const feature = new Feature(polygon);
+    const vectorSource = new VectorSource({ features: [feature] });
+    const layer = new VectorLayer({ source: vectorSource });
+    map?.addLayer(layer);
   };
 
   return { setNewCenterAndNewZoom, fitViewToPolygon };
